b43ce9515eab02d14722ef2ed7db20c66be1b76b
rochatadeu <tadeu.acr@gmail.com>
2016-11-28 22:45:23 -0500

    HW3-WIP

--- mcserver.py
+++ mcserver.py
@@ -1,8 +1,15 @@
-import ConfigParser, logging, datetime, os
+import ConfigParser, logging, datetime, os, json, requests
 
 from flask import Flask, render_template, request
 
 import mediacloud
+
+import chartkick
+
+from API_config import MY_API_KEY
+
+app=Flask(__name__,static_folder=chartkick.js(), static_url_path='/static/js/')
+app.jinja_env.add_extension("chartkick.ext.charts")
 
 CONFIG_FILE = 'settings.config'
 basedir = os.path.dirname(os.path.realpath(__file__))
@@ -28,13 +35,21 @@ def home():
 @app.route("/search",methods=['POST'])
 def search_results():
     keywords = request.form['keywords']
-    now = datetime.datetime.now()
-    results = mc.sentenceCount(keywords,
-        solr_filter=[mc.publish_date_query( datetime.date( 2015, 1, 1), 
-                                            datetime.date( now.year, now.month, now.day) ),
-                     'media_sets_id:1' ])
+    startdate_post = request.form['startdate']
+    enddate_post = request.form['enddate']
+
+    startdate = datetime.datetime.strptime(startdate_post, '%Y-%m-%d')
+    enddate = datetime.datetime.strptime(enddate_post, '%Y-%m-%d')
+
+    parameters = {'split':'1', 'split_start_date':startdate_post,'split_end_date':enddate_post, 'key':MY_API_KEY}
+
+    results = requests.get("https://api.mediacloud.org/api/v2/sentences/count?q=sentence:"+str(keywords)+"+AND+tags_id_media:8875027", params=parameters)
+    data = results.json()
+
     return render_template("search-results.html", 
-        keywords=keywords, sentenceCount=results['count'] )
+        keywords=keywords, startdate=startdate, enddate=enddate, sentenceCount=data['count'], data=data)
+
+
 
 if __name__ == "__main__":
     app.debug = True
